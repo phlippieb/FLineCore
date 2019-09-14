9359682cd59f33637ada4e2bbe99c3dfdf20ae4a
phlippieb <phlippie.bosman@gmail.com>
2019-09-14 19:53:44 +0200

    Create StoreFactory to create the ReSwift store

--- Tests/FLineCoreTests/FLineCoreTests.swift
+++ Tests/FLineCoreTests/FLineCoreTests.swift
@@ -12,3 +12,47 @@ final class TestSpec: QuickSpec {
         }
     }
 }
+
+final class StoreFactorySpec: QuickSpec {
+    override func spec() {
+        describe("The StoreFactory") {
+            func _makeSUT() -> StoreFactory {
+                return StoreFactory()
+            }
+            
+            it("can be instantiated") {
+                _ = _makeSUT()
+            }
+            
+            it("creates a store with a state.") {
+                let sut = _makeSUT()
+                expect(sut.createStore().state).toNot(beNil())
+            }
+        }
+    }
+}
+
+// // // // // // //
+// Implementation //
+// // // // // // //
+
+import ReSwift
+
+final class StoreFactory {
+    func createStore() -> Store<FirelineState> {
+        return Store(
+            reducer: firelineReducer,
+            state: FirelineState())
+    }
+}
+
+struct FirelineState: StateType {}
+
+let firelineReducer: Reducer<FirelineState> = _firelineReducer
+
+func _firelineReducer(
+    _ action: Action,
+    _ state: FirelineState?
+    ) -> FirelineState {
+    return state ?? FirelineState()
+}
