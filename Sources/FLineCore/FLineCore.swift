d8a0e639735ae53282393296588e11d51ee49563
phlippieb <phlippie.bosman@gmail.com>
2019-09-18 07:56:19 +0200

    New game state starts at game menu

--- Sources/FLineCore/FLineCore.swift
+++
@@ -1,3 +0,0 @@
-struct FLineCore {
-    var text = "Hello, World!"
-}
--- Sources/FLineCore/Store/Store.swift
+++ Sources/FLineCore/Store/Store.swift
@@ -15,8 +15,25 @@ final class StoreFactory {
     }
 }
 
-struct FirelineState: StateType {}
-
 let firelineReducer: Reducer<FirelineState> = { (action: Action, state: FirelineState?) in
     return state ?? FirelineState()
+}
+
+struct FirelineState: StateType {
+    init() {
+        self.mainScene = .gameMenu
+    }
+    
+    let mainScene: MainSceneType
+}
+
+enum MainSceneType {
+    case gameMenu
+    
+}
+
+enum GameMenuAction {
+    case play
+    case practice
+    case quit
 }
