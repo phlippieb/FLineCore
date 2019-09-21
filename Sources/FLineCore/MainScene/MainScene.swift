540a5defe6a7730cb1782ecb26456de4865c5653
phlippieb <phlippie.bosman@gmail.com>
2019-09-21 16:10:53 +0200

    Refactor: Move Playing code to Playing.swift

--- Sources/FLineCore/MainScene/MainScene.swift
+++ Sources/FLineCore/MainScene/MainScene.swift
@@ -3,29 +3,3 @@ public enum MainSceneType: Equatable {
     case playing(PlayingState)
 }
 
-public struct PlayingState: Equatable {
-    var tick: Int = 0
-}
-
-import ReSwift
-
-public enum PlayingAction: Action {
-    case tick
-}
-
-let playingReducer: Reducer<FirelineState> = { (action: Action, state: FirelineState?) -> FirelineState in
-   var state = state ?? FirelineState()
-
-    guard let playingAction = action as? PlayingAction,
-        case .playing(var playingState) = state.mainScene
-        else { return state }
-
-    switch playingAction {
-    case .tick:
-        playingState.tick += 1
-    }
-
-    state.mainScene = .playing(playingState)
-    return state
-}
-
---
+++ Sources/FLineCore/Playing/Playing.swift
@@ -0,0 +1,26 @@
+import ReSwift
+
+public struct PlayingState: Equatable {
+    var tick: Int = 0
+}
+
+public enum PlayingAction: Action {
+    case tick
+}
+
+let playingReducer: Reducer<FirelineState> = { (action: Action, state: FirelineState?) -> FirelineState in
+   var state = state ?? FirelineState()
+
+    guard let playingAction = action as? PlayingAction,
+        case .playing(var playingState) = state.mainScene
+        else { return state }
+
+    switch playingAction {
+    case .tick:
+        playingState.tick += 1
+    }
+
+    state.mainScene = .playing(playingState)
+    return state
+}
+
