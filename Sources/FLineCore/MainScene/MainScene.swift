102439f1e077802f23a57a1b2733f844056ed95b
phlippieb <phlippie.bosman@gmail.com>
2019-09-22 16:52:18 +0200

    Add playingState property to MainSceneType enum

--- Sources/FLineCore/MainScene/MainScene.swift
+++ Sources/FLineCore/MainScene/MainScene.swift
@@ -1,5 +1,21 @@
 public enum MainSceneType: Equatable {
     case mainMenu
     case playing(PlayingState)
+
+    public var mainMenu: Void? {
+        guard case .mainMenu = self else { return nil }
+        return ()
+    }
+
+    public var playingState: PlayingState? {
+        get {
+            guard case let .playing(value) = self else { return nil }
+            return value
+        }
+        set {
+            guard case .playing = self, let newValue = newValue else { return }
+            self = .playing(newValue)
+        }
+    }
 }
 
